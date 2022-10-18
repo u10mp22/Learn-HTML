561ce914679de0fde3accfdaf76e1f3b17011f8c
Michael Pollard <u10mp22@abdn.ac.uk>
2022-10-18 15:28:47 +0000

    Added javascript which is so cool!

---
+++ externaljs.js
@@ -0,0 +1,19 @@
+function dateandtime() {
+document.getElementById('date').innerHTML = Date()
+}
+function changetext() {
+document.getElementById("text").innerHTML = "You wasted a few milleseconds of your life clicking this button!"    
+}
+
+function whopper() {
+document.getElementById('myImage').src='whopper.jpg'
+}
+function bigmac() {
+document.getElementById('myImage').src='big mac.jpg'
+}
+function gone() {
+document.getElementById('myImage').style.display='none'
+}
+function appear() {
+document.getElementById('myImage').style.display='block'
+}
